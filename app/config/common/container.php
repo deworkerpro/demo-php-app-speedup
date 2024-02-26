a94a49f874e8ae3d68d3a7c9e39c1caf8aaf7625
ElisDN <mail@elisdn.ru>
2024-02-26 13:19:46 +0300

    Add config cache

---
+++ app/config/common/container.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+use Laminas\ConfigAggregator\ConfigAggregator;
+
+return [
+    ConfigAggregator::ENABLE_CACHE => true,
+];
--- app/config/dependencies.php
+++ app/config/dependencies.php
@@ -2,14 +2,20 @@
 
 declare(strict_types=1);
 
+use Laminas\ConfigAggregator\ArrayProvider;
 use Laminas\ConfigAggregator\ConfigAggregator;
 use Laminas\ConfigAggregator\PhpFileProvider;
 
 use function App\env;
 
+$cacheConfig = [
+    'config_cache_path' => __DIR__ . '/../var/cache/config-cache.php',
+];
+
 $aggregator = new ConfigAggregator([
     new PhpFileProvider(__DIR__ . '/common/*.php'),
+    new ArrayProvider($cacheConfig),
     new PhpFileProvider(__DIR__ . '/' . env('APP_ENV', 'prod') . '/*.php'),
-]);
+], $cacheConfig['config_cache_path']);
 
 return $aggregator->getMergedConfig();
