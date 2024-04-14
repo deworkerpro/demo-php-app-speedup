d846d9ea744ec7d42f9e0e626f97627a29416b9e
ElisDN <mail@elisdn.ru>
2024-04-15 00:40:39 +0300

    Move app out of loop

--- app/public/index.php
+++ app/public/index.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use App\Http\Response\EmptyResponse;
+use Doctrine\ORM\EntityManagerInterface;
 use Slim\Psr7\Factory\ServerRequestFactory;
 use Slim\Psr7\Factory\StreamFactory;
 use Slim\Psr7\Factory\UploadedFileFactory;
@@ -31,7 +32,16 @@ $config = require __DIR__ . '/../config/dependencies.php';
 $createContainer = require __DIR__ . '/../config/container.php';
 $createApp = require __DIR__ . '/../config/app.php';
 
+$container = $createContainer($config);
+$app = $createApp($container);
+
+const REQUESTS_PER_INSTANCE = 100;
+
+$count = 0;
+
 while (true) {
+    ++$count;
+
     try {
         $request = $psr7->waitRequest();
         if ($request === null) {
@@ -44,12 +54,23 @@ while (true) {
     }
 
     try {
-        $app = $createApp($createContainer($config));
+        $container->get(EntityManagerInterface::class)->clear();
+
         $response = $app->handle($request);
         $psr7->respond($response);
+
+        if ($response->getStatusCode() === 500 || $count >= REQUESTS_PER_INSTANCE) {
+            $count = 0;
+            $container = $createContainer($config);
+            $app = $createApp($container);
+        }
     } catch (Throwable $e) {
         $psr7->respond(new EmptyResponse(500));
         $psr7->getWorker()->error((string)$e);
         Sentry\captureException($e);
+
+        $count = 0;
+        $container = $createContainer($config);
+        $app = $createApp($container);
     }
 }
